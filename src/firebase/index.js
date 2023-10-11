9f4033edf4307dc750f0c2bd149e27588856be9f
Fredes Virginia <94409602+FredesVirginia@users.noreply.github.com>
2023-10-11 16:17:20 +0000

    Provar esto

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,11 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getMessaging , getToken} from "firebase/messaging";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyA_CE84Bo8bies8EjQwhzCMYaPBwqIFHjY",
   authDomain: "firefise-shooping.firebaseapp.com",
@@ -19,6 +24,20 @@ const firebaseConfig = {
 // Initialize Firebase
  const app = initializeApp(firebaseConfig);
 
-const analytics = getAnalytics(app);
+ const messaging = getMessaging();
+ getToken(messaging, { vapidKey : "BETlaoi-RhzF7UspHOJl2sUvah9xQh_hWJtulY9x8mleV_Kgmh8pQkg6H"}).then((currentToken) => {
+   if (currentToken) {
+     // Send the token to your server and update the UI if necessary
+     // ...
+     console.log("El current token es " , currentToken);
+   } else {
+     // Show permission request UI
+     console.log('No registration token available. Request permission to generate one.');
+     // ...
+   }
+ }).catch((err) => {
+   console.log('An error occurred while retrieving token. ', err);
+   // ...
+ });
 
 
